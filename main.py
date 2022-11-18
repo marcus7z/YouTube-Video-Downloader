7e324f0e9fbfab96a09d409839839f5d46516119
Ahmet Öner <ahmetoner315@gmail.com>
2022-11-18 21:02:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from pytube import YouTube
+from tqdm import tqdm
+import threading
+  
+def DownloadFromYT(stream): 
+    stream.download()
+
+def DownloadProcess(size):
+    length = size
+    generator = (3 * n for n in range(length))  
+    for n in tqdm(generator, total=length):
+        pass
+    
 link = str(input("Enter Your Link: "))
+ 
+ 
 yt = YouTube(link)
 stream = yt.streams.get_highest_resolution()
-stream.download()
+file_name = yt.title
+filesize = yt.streams.get_highest_resolution().filesize  
 
-from tqdm import tqdm
- 
- 
-for i in tqdm(range(int(9e6))):
-    pass
+t1 = threading.Thread(target=DownloadFromYT, args=(stream,))
+t2 = threading.Thread(target=DownloadProcess,args=(filesize,))
+  
+t1.start() 
+t2.start()
+  
+t1.join() 
+t2.join()
 
-
-print("DOWNLOADED !!!!")
+print("DOWNLOADED !! %s" % file_name)
