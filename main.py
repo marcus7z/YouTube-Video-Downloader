8aabe2b8cba64407ef5d528028e1f05578bd0d9e
Ahmet Öner <ahmetoner315@gmail.com>
2022-11-18 19:31:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ link = str(input("Enter Your Link: "))
 yt = YouTube(link)
 stream = yt.streams.get_highest_resolution()
 stream.download()
-print("Download")
+
+from tqdm import tqdm
+ 
+ 
+for i in tqdm(range(int(9e6))):
+    pass
+
+
+print("DOWNLOADED !!!!")
